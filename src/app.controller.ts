8408bfb12e801c340103c9587543403d3daa3db7
Ayan De <deayan252@gmail.com>
2025-07-19 22:06:53 +0530

    swagger docs setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +12,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Hello World!' })
+  @ApiOkResponse({ description: 'Everything is ok' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @ApiResponse({ status: 200, description: 'Hello World!' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthProtected } from './protected/auth.protected';
 
@@ -8,6 +22,9 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @ApiOperation({ summary: 'Login' })
+  @ApiCreatedResponse({ description: 'Success' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   login(@Body() input: { username: string; password: string }) {
     return this.authService.authenticate(input);
   }
