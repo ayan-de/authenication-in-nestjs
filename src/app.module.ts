684fcfbb487cf89ebf056848a91d6e63347562f4
ayan-de <deayan@gmail.com>
2025-07-24 17:58:28 +0530

    feat: mongo db atlas connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+dotenv.config();
 
 @Module({
   imports: [
@@ -15,7 +17,7 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: 'mongodb://localhost:27017/nestjs-type-orm',
+      url: process.env.MONGO_DB_URL,
       synchronize: true,
       entities: ['dist/**/*.entity.js'],
       database: 'nestjs-type-orm',
